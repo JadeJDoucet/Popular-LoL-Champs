f042676b1493ffb5a6f4472ec1c90168a6fc00ff
Jade <jadejdoucet@gmail.com>
2019-10-21 12:06:21 -0500

    added riot.js to handle api reqs to riot, imported kayn library

---
+++ server/riot.js
@@ -0,0 +1,28 @@
+const { Kayn, REGIONS } = require('kayn');
+
+const kayn = Kayn('RGAPI-my-api-key')(/* {
+    region: REGIONS.NORTH_AMERICA,
+    apiURLPrefix: 'https://%s.api.riotgames.com',
+    locale: 'en_US',
+    debugOptions: {
+        isEnabled: true,
+        showKey: false,
+    },
+    requestOptions: {
+        shouldRetry: true,
+        numberOfRetriesBeforeAbort: 3,
+        delayBeforeRetry: 1000,
+        burst: false,
+        shouldExitOn403: false,
+    },
+    cacheOptions: {
+        cache: null,
+        timeToLives: {
+            useDefault: false,
+            byGroup: {},
+            byMethod: {},
+        },
+    },
+} */);
+// handle requests to league of legends here
+// maybe require kayn here
