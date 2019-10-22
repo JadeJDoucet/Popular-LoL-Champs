725b86eaa20aa9bd3f310aeea16bf1ef901337ec
Jade <jadejdoucet@gmail.com>
2019-10-22 17:40:14 -0500

    rendering champion images dynamically

--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-const ListItem = ({ item }) => (
-  <div>
+// add image tag to each list item with item.source?
+const ListItem = ({ item }) => {
+  console.log(item);
+  return (
+    <div>
+    {/* <img src="Aatrox_0.jpg" /> */}
+    <img src={`/images/tiles/${item.championName}_0.jpg`} alt="Character Portrait" />
     <li> <b>{item.championName} </b>
       <p><b>Estimated Games: </b>{item.quantity}</p>
     </li>
   </div>
-);
+  )
+; };
 
 ListItem.propTypes = {
   item: PropTypes.shape({
