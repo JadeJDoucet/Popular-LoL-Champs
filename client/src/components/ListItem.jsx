3a661d59df9b4a8ae30f48782a4bbf4bc96a14ff
Jade <jadejdoucet@gmail.com>
2019-10-25 17:36:52 -0500

    added conditional for leblanc bug

--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -7,7 +7,9 @@ const ListItem = ({ item }) => {
   } else
     if (item.championName === "Kai'Sa") { // reassigning these to load proper image
       item.championName = 'Kaisa';
-    } 
+    } else if (item.championName === 'LeBlanc') {
+      item.championName = 'Leblanc';
+    }
   item.championName = item.championName.replace(/ +/g, "");
   return (
     <li>
